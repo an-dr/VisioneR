4876480a5afca6bcaa1327edd96d45c3fe1e054b
Andrei Gramakov <mail@agramakov.me>
2024-03-16 14:35:15 +0100

    Add prototype showing faces and blinking

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,6 @@
 // https://docs.opencv.org/3.4/d7/dff/tutorial_feature_homography.html
 
+#include <unistd.h>
 #include <opencv2/calib3d/calib3d.hpp> // for homography
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
@@ -10,21 +11,66 @@
 
 #include "ObjectFinder.hpp"
 
+using namespace cv;
+
 void proto2()
 {
-    cv::Mat objectImg = cv::imread("img_obj.jpg");
-    cv::Mat sceneImg = cv::imread("img_scene_many.jpg");
+    Mat objectImg = imread("img_obj.jpg");
+    Mat sceneImg = imread("img_scene_many.jpg");
 
     ObjectFinder of;
     of.SetScene(sceneImg);
 
-    cv::Point2f result;
+    Point2f result;
     of.Find(objectImg, result);
     printf("Result: %f %f\n", result.x, result.y);
 }
 
+void wait(int delay, bool &exit)
+{
+    if (waitKey(delay) == 27) // ESC
+    {
+        exit = true;
+    }
+}
+
+void proto_faces()
+{
+
+    Mat blink = imread("assets/blink.png");
+    Mat calm = imread("assets/calm.png");
+    Mat happy = imread("assets/happy.png");
+    Mat sad = imread("assets/sad.png");
+    Mat confused = imread("assets/confused.png");
+    const char *windowName = "VisioneR";
+
+    namedWindow(windowName, WINDOW_GUI_EXPANDED);
+
+    bool exit = false;
+    
+    imshow(windowName, calm);
+    resizeWindow(windowName, 500, 128);
+    
+    wait(1000, exit);
+    imshow(windowName, happy);
+    wait(1000, exit);
+    imshow(windowName, sad);
+    wait(1000, exit);
+    imshow(windowName, confused);
+    wait(1000, exit);
+    imshow(windowName, blink);
+    while (!exit)
+    {
+        imshow(windowName, calm);
+        wait(2000, exit);
+
+        imshow(windowName, blink);
+        wait(300, exit);
+    }
+}
+
 int main(int argc, char **argv)
 {
-    proto2();
+    proto_faces();
     return 0;
 }
